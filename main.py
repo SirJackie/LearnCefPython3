d2552dc31c9915974eff1a428f570b7910164034
lyj00912 <3122742760@qq.com>
2022-01-22 22:52:33 +0800

    finally successfully opened a local HTML file

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from cefpython3 import cefpython as cef
 import sys
+import os
 
-# 替换python预定义异常处理逻辑，为保证异常发生时能够结束所有进程
+# file:///C:/Users/Admin/Desktop/Projects/learn-cefpython3/HTMLSourceCodes/index.html
+
+url = "HTMLSourceCodes/index.html"
+cwd = "file:///" + os.getcwd().replace("\\", "/") + "/"
+targetURL = cwd + url
+print(targetURL)
+
 sys.excepthook = cef.ExceptHook
-
-# 创建浏览器
-from cefpython3 import cefpython as cef
-
 cef.Initialize()
-browser = cef.CreateBrowserSync(url="http://www.bilibili.com/")
-
+browser = cef.CreateBrowserSync(url=targetURL)
 browser.ExecuteJavascript("alert(\"Hello World!\")")
-
-# 消息循环：监听信号和处理事件
 cef.MessageLoop()
-
-# 结束进程
 cef.Shutdown()

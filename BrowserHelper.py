ff52936ea8306296b24d7be1ba7b9c2cf27717b1
lyj00912 <3122742760@qq.com>
2022-01-23 08:34:53 +0800

    made url process more robust

---
+++ BrowserHelper.py
@@ -0,0 +1,31 @@
+from cefpython3 import cefpython as cef
+import sys
+import os
+
+
+def LocalizeURL(URL):
+    # If the URL is based on HTTP or HTTPS protocol
+    if URL[0:4] == "http":
+        # Just return, don't process
+        return URL
+
+    # Linuxlize the URL
+    linuxlizedURL = URL.replace("\\", "/")
+
+    # Clean the 'this folder' definition
+    if linuxlizedURL[0:2] == "./":
+        linuxlizedURL = linuxlizedURL[2:]
+
+    # Get Linuxlized CWD Path
+    linuxlizedCWD = os.getcwd().replace("\\", "/")
+
+    # Merge and Generate the Target URL
+    targetURL = "file:///" + linuxlizedCWD + "/" + linuxlizedURL
+    return targetURL
+
+
+def CreateBrowser(URL):
+    sys.excepthook = cef.ExceptHook
+    cef.Initialize()
+    browser = cef.CreateBrowserSync(url=LocalizeURL(URL))
+    return browser
--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from cefpython3 import cefpython as cef
-import sys
-import os
+from BrowserHelper import *
 
-# file:///C:/Users/Admin/Desktop/Projects/learn-cefpython3/HTMLSourceCodes/index.html
-
-url = "HTMLSourceCodes/index.html"
-cwd = "file:///" + os.getcwd().replace("\\", "/") + "/"
-targetURL = cwd + url
-print(targetURL)
-
-sys.excepthook = cef.ExceptHook
-cef.Initialize()
-browser = cef.CreateBrowserSync(url=targetURL)
+browser = CreateBrowser("./HTMLSourceCodes/index.html")
 browser.ExecuteJavascript("alert(\"Hello World!\")")
 cef.MessageLoop()
 cef.Shutdown()

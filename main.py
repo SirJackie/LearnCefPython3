9e944ffb75222776d07d6c0a8bcec772f24bb554
lyj00912 <3122742760@qq.com>
2022-01-24 21:06:16 +0800

    further refactored

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from cefpython3 import cefpython as cef
 from BrowserHelper import *
-import win32gui
-import win32api
-import ctypes
-
-cef.DpiAware.EnableHighDpiSupport()
+from WindowHelper import *
 
 
 # 关于浏览器事件的客户端处理器
@@ -38,38 +34,8 @@ def PythonFunction():
     print("Python received a call from JS for the", str(count) + "th time.")
 
 
+EnableHighDPISupport()
 browser = CreateBrowser("./HTMLSourceCodes/index.html")
-
-
-def GetTaskBarHeight():
-    monitorInfo = win32api.GetMonitorInfo(win32api.MonitorFromPoint((0, 0)))
-    monitorArea = monitorInfo.get("Monitor")
-    workArea = monitorInfo.get("Work")
-    taskbarHeight = monitorArea[3]-workArea[3]
-    return taskbarHeight
-
-
-def GetScreenSize():
-    user32 = ctypes.windll.user32
-    screenWidth = user32.GetSystemMetrics(0)
-    screenHeight = user32.GetSystemMetrics(1)
-    return screenWidth, screenHeight
-
-
-def SetWindowSizeAndPos(width, height, left, top):
-    hwnd = win32gui.GetForegroundWindow()
-    win32gui.MoveWindow(hwnd, left, top, width, height, True)
-
-
-def GetLeftAndTopUsingWidthAndHeight(windowWidth, windowHeight):
-    screenWidth, screenHeight = GetScreenSize()
-    taskbarHeight = GetTaskBarHeight()
-
-    windowLeft = int((screenWidth - windowWidth) / 2)
-    windowTop = int(((screenHeight - taskbarHeight) - windowHeight) / 2)
-
-    return windowLeft, windowTop
-
 
 screenWidth, screenHeight = GetScreenSize()
 taskbarHeight = GetTaskBarHeight()

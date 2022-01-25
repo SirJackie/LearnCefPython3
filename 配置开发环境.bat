c66be5a62417f3b53f9bdac2eba62fac24751f47
lyj00912 <3122742760@qq.com>
2022-01-25 19:27:17 +0800

    modified the environment builder script, make it more compatible

--- 配置开发环境.bat
+++ 配置开发环境.bat
@@ -1,6 +1,3 @@
-@echo off
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-cd /d "%~dp0"
 pip install cefpython3 -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple
 pip install pywin32 -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple
 echo 已完成开发环境配置，按任意键退出程序。
